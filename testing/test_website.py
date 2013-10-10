ed154938b19317f8c0848ef9fd8d4869956bc348
Warren Mar <warren.mar@gmail.com>
2013-10-09 23:07:06 -0700

    test for utf8-json

--- testing/test_website.py
+++ testing/test_website.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from requests import session
 import json
 import selenium
@@ -13,38 +15,41 @@ DOMAIN = 'localhost'
 def epoch_now():
     return time.mktime(time.gmtime())
 
+def main():
+    # create account with fb
+    with session() as c:
+        d = { "id" : '123456',
+                    "first_name" : 'Frëd',
+                    "last_name" : 'Low',
+                    "email" : 'fredlow340@yahoo.com',
+                    "location" : 'Mountain View, CA' }
+        headers = {'content-type': 'application/json; charset=utf-8json'}
+        request = c.post('http://localhost:8080/signup/fb', data=json.dumps(d), headers=headers)
 
-with session() as c:
-    d = { "id" : '12345',
-                "first_name" : 'Fred',
-                "last_name" : 'Low',
-                "email" : 'fredlow340@yahoo.com',
-                "location" : 'Mountain View, CA' }
-    headers = {'content-type': 'application/json'}
-    request = c.post('http://localhost:8080/signup/fb', data=json.dumps(d), headers=headers)
+    driver = webdriver.Firefox()
+    driver.get("http://127.0.0.1:8080/")
+    driver.add_cookie({'name':'key', 'value':'value', 'path':'/'})
+    for cookie in request.cookies:
+        driver.add_cookie({'name' : cookie.name, 'value': cookie.value, 'path':'/', 'domain' : '127.0.0.1'})
 
-driver = webdriver.Firefox()
-driver.get("http://127.0.0.1:8080/")
-driver.add_cookie({'name':'key', 'value':'value', 'path':'/'})
-for cookie in request.cookies:
-    driver.add_cookie({'name' : cookie.name, 'value': cookie.value, 'path':'/', 'domain' : '127.0.0.1'})
+    all_cookies = driver.get_cookies()
 
-all_cookies = driver.get_cookies()
-
-driver.get("http://127.0.0.1:8080/")
-driver.save_screenshot('screenshot-%i.png' % epoch_now())
-time.sleep(3)
-
-for navitem in ['navicoreqs', 'navicoroutes', 'navicosearch']:
-    elem = driver.find_element_by_class_name(navitem)
-    elem.click()
+    driver.get("http://127.0.0.1:8080/")
     driver.save_screenshot('screenshot-%i.png' % epoch_now())
     time.sleep(3)
 
-#navicoroutes
-#navicosearch
+    for navitem in ['navicoreqs', 'navicoroutes', 'navicosearch']:
+        elem = driver.find_element_by_class_name(navitem)
+        elem.click()
+        driver.save_screenshot('screenshot-%i.png' % epoch_now())
+        time.sleep(3)
 
-#driver.get("http://www.gobarnacle.com/post/request")
+    #navicoroutes
+    #navicosearch
 
-driver.close()
+    #driver.get("http://www.gobarnacle.com/post/request")
 
+    driver.close()
+
+if __name__ == "__main__":
+    main()
