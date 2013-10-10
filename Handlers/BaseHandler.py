ff5db8a4c79eacbdfd78870277445126c788a6d5
Warren Mar <warren.mar@gmail.com>
2013-10-09 23:17:52 -0700

    fix for utf-8json

--- Handlers/BaseHandler.py
+++ Handlers/BaseHandler.py
@@ -13,8 +13,8 @@ jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader('templates'), au
 class BaseHandler(webapp2.RequestHandler):
     """ BaseHandler, generic helper functions and user handling """
     current_user_key = None
-    user_prefs = None # preferences for logged in user    
-    
+    user_prefs = None # preferences for logged in user
+
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
 
@@ -32,7 +32,10 @@ class BaseHandler(webapp2.RequestHandler):
 
     def initialize(self, *a, **kw):
         """ called every time """
-        webapp2.RequestHandler.initialize(self, *a, **kw) 
+        webapp2.RequestHandler.initialize(self, *a, **kw)
+        # webapp2 does not handle utf-8json encoding from facebook
+        if self.request.charset == 'utf-8json':
+            self.request.charset = 'utf-8'
         self.params = {} # parameters to pass to template renderer
         self.set_current_user()
         self.fill_header()
@@ -55,7 +58,7 @@ class BaseHandler(webapp2.RequestHandler):
         elif acct:
             self.set_secure_cookie('account', acct)
             self.set_secure_cookie('user_id', str(email))
-            
+
     def logout(self):
         """ logout by deleting cookie """
         self.response.headers.add_header('Set-Cookie', 'user_id=; Path=/')
@@ -69,18 +72,18 @@ class BaseHandler(webapp2.RequestHandler):
             if not up:
                 up = UserPrefs(userid = user.user_id(), account_type = 'google')
                 up.put()
-            #set cookie
+            # set cookie
             logacct = self.read_secure_cookie('account')
             if logacct=='':
                 self.set_secure_cookie('account', 'google')
                 self.set_secure_cookie('user_id', str(up.userid))
             self.user_prefs = up
             self.current_user_key = up.key
-        #otherwise it's a p2p account or facebook
+        # otherwise it's a p2p account or facebook
         email = self.read_secure_cookie('user_id')
         if email:
             up = UserPrefs.by_userid(email)
-            if up:            
+            if up:
                 self.user_prefs = up
                 self.current_user_key = up.key
         elif self.request.get('login_token'):
@@ -88,13 +91,14 @@ class BaseHandler(webapp2.RequestHandler):
             if user:
                 self.user_prefs = UserPrefs.by_email(user.email)
                 self.current_user_key = self.user_prefs.key
-                
+
     def fill_header(self):
         """ fill in header information params """
         if self.user_prefs:
             self.params.update(gen_header(self.user_prefs))
         else:
             self.params.update(gen_header(None))
+
 
 def gen_header(up=None):
     header = {}
@@ -105,4 +109,4 @@ def gen_header(up=None):
         header['prof_thumb'] = up.profile_image_url('small')
     else:
         header['prof_thumb'] = blank_img
-    return header            
+    return header
