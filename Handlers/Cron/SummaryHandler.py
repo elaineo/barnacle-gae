154614eae72b7ea16122f37867fc7b4a3ab0893b
Warren Mar <warren.mar@gmail.com>
2014-03-19 22:20:11 -0700

    Add new messages to daily summary

--- Handlers/Cron/SummaryHandler.py
+++ Handlers/Cron/SummaryHandler.py
@@ -8,6 +8,7 @@ from Models.Message import Message
 from datetime import datetime, timedelta
 from google.appengine.ext import ndb
 
+
 class SummaryHandler(BaseHandler):
     def day_ago(self):
         return datetime.now() - timedelta(1)
@@ -40,8 +41,8 @@ class SummaryHandler(BaseHandler):
         return Request.query(Request.created > self.day_ago()).filter(Request.dead==0).count()
 
     def number_of_users(self):
-        return UserPrefs.query().count()                
-        
+        return UserPrefs.query().count()
+
     def number_of_routes(self):
         return Route.query(Route.dead==0).count()
 
@@ -69,6 +70,7 @@ class SummaryHandler(BaseHandler):
         d['new_routes'] = self.number_of_new_routes()
         d['new_requests'] = self.number_of_new_requests()
         d['new_reservations'] = self.number_of_new_reservations()
+        d['new_messages'] = self.number_of_new_messages()
         buf = 'In Past Day\n'
         buf += '\n'.join([k + ' : ' + str(d[k]) for k in d]) + '\n'
         buf += '\nNew Users\n' + self.new_users_report()
@@ -85,21 +87,21 @@ class SummaryHandler(BaseHandler):
         if not action:
             body = self.generate_report()
             timestamp = datetime.now().strftime('%Y/%m/%d')
-            subject = 'Daily Summary for %s' % timestamp            
+            subject = 'Daily Summary for %s' % timestamp
             self.email_report('warren.mar@gmail.com', subject, body)
             self.email_report('help@gobarnacle.com', subject, body)
         elif action=='selfnote' and key:
             r = ndb.Key(urlsafe=key).get()
             if r.details:
-                buf = r.details + ' from ' + r.locstart + ' to ' + r.locend 
+                buf = r.details + ' from ' + r.locstart + ' to ' + r.locend
             else:
-                buf = r.locstart + ' to ' + r.locend 
+                buf = r.locstart + ' to ' + r.locend
             buf += '\n http://www.gobarnacle.com' + r.post_url()
             subject = 'New Barnacle request'
             self.email_report('help@gobarnacle.com', subject, buf)
         elif action=='selfpay' and key:
             r = ndb.Key(urlsafe=key).get()
-            buf = r.fullname() + ' tried to give us a cc' 
+            buf = r.fullname() + ' tried to give us a cc'
             buf += '\n http://www.gobarnacle.com' + r.profile_url()
             subject = 'New Barnacle payment account'
-            self.email_report('help@gobarnacle.com', subject, buf)            
+            self.email_report('help@gobarnacle.com', subject, buf)
