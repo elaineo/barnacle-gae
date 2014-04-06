858b3c31bc0df20677baad4bcd6d13b196bd2063
Warren Mar <warren.mar@gmail.com>
2014-04-06 15:07:21 -0700

    fix bug for no last name in account profile

--- Models/User/Account.py
+++ Models/User/Account.py
@@ -9,45 +9,45 @@ import urllib2
 
 # Models #
 ##########
-    
+
 class UserStats(ndb.Model):
     referral = ndb.StringProperty()
     code = ndb.StringProperty()
     ip_addr = ndb.StringProperty(repeated=True)
     locations = ndb.StringProperty(repeated=True)
-    
+
 class UserSettings(ndb.Model):
-    """ 0.Messages 1.Reservation updates 2.Review notify 3.Matching Delivery requests    4. Newsletter """ 
-    notify = ndb.IntegerProperty(repeated=True) 
+    """ 0.Messages 1.Reservation updates 2.Review notify 3.Matching Delivery requests    4. Newsletter """
+    notify = ndb.IntegerProperty(repeated=True)
 
 class UserAccount(ndb.Model):
-    email = ndb.StringProperty() 
-    tel = ndb.StringProperty() 
-    name = ndb.StringProperty() 
+    email = ndb.StringProperty()
+    tel = ndb.StringProperty()
+    name = ndb.StringProperty()
     location = ndb.StringProperty()
     locpt = ndb.GeoPtProperty(default=ndb.GeoPt(37.4,-122))
     usertype = ndb.IntegerProperty(default=2)
-    stats = ndb.StructuredProperty(UserStats) 
-    
+    stats = ndb.StructuredProperty(UserStats)
+
 class UserPrefs(ndb.Model):
     """ Individual User data fields """
     account_type = ndb.StringProperty(default='fb') #p2p or social network
     userid = ndb.StringProperty() # fb unique identifier
-    email = ndb.StringProperty() 
-    tel = ndb.StringProperty() 
+    email = ndb.StringProperty()
+    tel = ndb.StringProperty()
     cust_id = ndb.StringProperty() # customer payment ID
     cc = ndb.StringProperty()
     first_name = ndb.StringProperty()
     last_name = ndb.StringProperty()
     location = ndb.StringProperty()
-    fblocation = ndb.StringProperty()    
+    fblocation = ndb.StringProperty()
     locpt = ndb.GeoPtProperty(default=ndb.GeoPt(37.4,-122))
     about = ndb.TextProperty()
     last_active = ndb.DateTimeProperty(auto_now=True)
     creation_date = ndb.DateTimeProperty(auto_now_add=True)
     img_id = ndb.IntegerProperty()      # set to -1 if using FB picture
     settings = ndb.StructuredProperty(UserSettings)
-    stats = ndb.StructuredProperty(UserStats) 
+    stats = ndb.StructuredProperty(UserStats)
     def fullname(self):
         if self.first_name and self.last_name:
             return self.first_name + ' ' + self.last_name
@@ -87,7 +87,7 @@ class UserPrefs(ndb.Model):
 
     def profile_url(self):
         return '/profile/' + self.key.urlsafe()
-    
+
     def get_notify(self):
         if self.settings:
             return self.settings.notify
@@ -103,12 +103,12 @@ class UserPrefs(ndb.Model):
         if self.last_name:
             params['last_name'] = self.last_name
         else:
-            paramsp['last_name'] = ''
-        params['profile_thumb'] = self.profile_image_url(mode='small')   
+            params['last_name'] = ''
+        params['profile_thumb'] = self.profile_image_url(mode='small')
         params['profile_url'] = self.profile_url()
         params['avg_rating'] = Review.avg_rating(self.key)
         if params['avg_rating'] < 1:
-            params['avg_rating'] = 5        
+            params['avg_rating'] = 5
         params['fbid'] = self.userid
         params['userkey'] = self.key.urlsafe()
         if self.cc:
@@ -116,12 +116,12 @@ class UserPrefs(ndb.Model):
         else:
             params['cc']=False
         return params
-    
+
     def params_fill(self):
         params = self.params_fill_sm()
         if self.last_name:
             params['last_name'] = self.last_name
-        else: 
+        else:
             params['last_name'] = ''
         if self.location:
             params['location'] = self.location
@@ -142,13 +142,13 @@ class UserPrefs(ndb.Model):
         if self.cc:
             params['cc']=True
         else:
-            params['cc']=False            
+            params['cc']=False
         params['profile_full'] = self.profile_image_url()
         params['review_json'] = '/review/json/'+self.key.urlsafe()
         params['deliv_completed'] = Transaction.deliveries_completed(self.key)
         params['notify'] = {'notify' : self.get_notify() }
         return params
-        
+
     @classmethod
     def by_userid(cls, email):
         q = cls.query(cls.userid == email).get()
@@ -157,4 +157,4 @@ class UserPrefs(ndb.Model):
     @classmethod
     def by_email(cls, email):
         u = cls.query(cls.email == email).get()
-        return u        
+        return u
